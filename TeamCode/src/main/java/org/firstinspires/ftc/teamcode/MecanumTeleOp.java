11abe1248a0e1b15da17b99a406d0c7dce256954
Sarea-T <tamsarea@gmail.com>
2025-10-13 18:33:25 -0700

    Merged shooter code with drive code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -4,38 +4,90 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
-@TeleOp(name = "SimpleMecanumDrive", group="Linear OpMode")
+@TeleOp(name = "2025TeleOp", group="Linear OpMode")
 public class MecanumTeleOp extends LinearOpMode {
+    //Shooters start at 10% power
+    double leftShooterPower = .1;
+    double rightShooterPower = .1;
+    public void increasePower() {
+        leftShooterPower = leftShooterPower + 0.1;
+        rightShooterPower = rightShooterPower + 0.1;
+        leftShooterPower = Math.min(leftShooterPower, 1.0);
+        rightShooterPower = Math.min(rightShooterPower, 1.0);
+    }
+    public void decreasePower() {
+        leftShooterPower = leftShooterPower - 0.1;
+        rightShooterPower = rightShooterPower - 0.1;
+        leftShooterPower = Math.max(leftShooterPower, 0);
+        rightShooterPower = Math.max(rightShooterPower, 0);
+    }
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor leftShooterMotor = hardwareMap.dcMotor.get("leftShooterMotor");
+        DcMotor rightShooterMotor = hardwareMap.dcMotor.get("rightShooterMotor");
 
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightShooterMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad previousGamepad1 = new Gamepad();
+
         while (opModeIsActive()) {
+            //The Y button is for shooting
+            //Use the Dpad to change the speed of the motors
+            boolean shoot = gamepad1.y;
+            boolean more = gamepad1.dpad_up;
+            boolean less = gamepad1.dpad_down;
+
+            previousGamepad1.copy(currentGamepad1);
+            currentGamepad1.copy(gamepad1);
+
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x * 1.1;
-            double rx = gamepad1.right_stick_x;
+            double rx = -gamepad1.right_stick_x;
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
+            double frontLeftPower = (y - x + rx) / denominator;
+            double backLeftPower = (y + x + rx) / denominator;
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
+
+            if (shoot == true) {
+                leftShooterMotor.setPower(leftShooterPower);
+                rightShooterMotor.setPower(rightShooterPower);
+            }else {
+                leftShooterMotor.setPower(0);
+                rightShooterMotor.setPower(0);
+            }
+
+            //Shooter power increases by 10% everytime Dpad up is pressed
+            if (currentGamepad1.dpad_up && !previousGamepad1.dpad_up) {
+                increasePower();
+            }
+
+            //Shooter power decreases by 10% everytime Dpad down is pressed
+            if (currentGamepad1.dpad_down && !previousGamepad1.dpad_down) {
+                decreasePower();
+            }
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+
+            telemetry.addData("motors", "frontLeft(%.2f) frontRight(%.2f) backLeft(%.2f) backRight(%.2f) leftShooter(%.2f) rightShooter(%.2f)", frontLeftPower, frontRightPower, backLeftPower, backRightPower, leftShooterPower, rightShooterPower);
+            telemetry.update();
         }
     }
 }
