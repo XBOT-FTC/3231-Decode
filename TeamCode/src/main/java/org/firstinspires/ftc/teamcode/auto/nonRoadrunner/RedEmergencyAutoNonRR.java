de212f28e29854472fe42fd26a7d2095ab60324f
David Nguyen <infernaldestinymc@gmail.com>
2025-10-30 18:40:54 -0700

    WIP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/nonRoadrunner/RedEmergencyAutoNonRR.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.auto.nonRoadrunner;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class RedEmergencyAutoNonRR extends LinearOpMode {
+    @Override
+    public void runOpMode(){
+        
+    }
+}
